fdba778608ea3764296d3c4d69cfc46138630bcf
Maksym-Svyrydov <110718671+Maksym-Svyrydov@users.noreply.github.com>
2022-12-13 00:33:17 +0200

    v 1.03

--- js/task-08.js
+++ js/task-08.js
@@ -1,21 +1,20 @@
 const formEl = document.querySelector(".login-form");
-console.log(formEl);
+
+// console.log(formEl);
 
 formEl.addEventListener("submit", checkFormInput);
 function checkFormInput(evt) {
-  let newUser = {
-    email,
-    password,
-  };
   evt.preventDefault();
   const {
     elements: { email, password },
   } = evt.currentTarget;
   if (email.value === "" || password.value === "") {
     return alert("Незаполнены поля!");
-  } else {
-    newUser.email = email.value;
-    newUser.password = password.value;
+  }
+  if (email.value && password.value) {
+    const email = formEl.email.value;
+    const password = formEl.password.value;
+    const newUser = { email, password };
 
     console.log(newUser);
 
